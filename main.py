8ba3bf0ede02d138ced430d7df372fcbf2038e89
toaro-moto <teddykharrim@gmail.com>
2025-09-29 23:18:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -303,5 +303,5 @@ def edit_programme(programme_id: int, data: EditProgramme, user=Depends(verify_t
 # ---------------- MAIN ENTRY ----------------
 if __name__ == "__main__":
     import uvicorn, os
-    port = int(os.environ.get("PORT",8000))
+    port = int(os.environ.get("PORT", 8000))   # Railway assigns the port
     uvicorn.run("main:app", host="0.0.0.0", port=port)
